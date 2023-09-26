a26fc0e22f9c0e595040da7b121f0f3d01294e2a
Reynalde Segerie <Reynalde.SEGERIE@etu.uca.fr>
2023-09-26 11:53:24 +0200

    Update files .c

--- imath/math_utility.h
+++ imath/math_utility.h
@@ -1,6 +1,9 @@
 #ifndef __MATH_UTILITY__
 #define __MATH_UTILITY__
 
+
+#define M_PI (3.14159265358979323846)
+
 double * calculateProbabilities(const double * array, int size, int sizeSample);
 double * calculateCumulativeProbabilities(const double * probaArray, int size);
 
--- mt19937ar.c
+++ mt19937ar.c
@@ -24,14 +24,14 @@ void test_empirical_distributions()
 int main(void)
 {
     // 2 - Generation of uniform random numbers between A and B
-    test_uniform(-89.2, 56.7);
+    //test_uniform(-89.2, 56.7);
     
     // 3 - Reproduction of discrete empirical distributions
-    test_empirical_distributions();
+    //test_empirical_distributions();
     
     // HDL Probabilities
     printf("\n -- HDL Probabilities calculation -- \n");
-    const double HDLarray[6] = {100, 400, 600, 400, 100, 200};
+    /*const double HDLarray[6] = {100, 400, 600, 400, 100, 200};
     double * HDLproba        = calculateProbabilities(HDLarray, 6, 1800);
     displayArray(HDLproba, 6);
 
@@ -64,7 +64,7 @@ int main(void)
 
 
     // 4 - Reproduction of continuous distributions
-    double r;
+    /*double r;
 
     double mean = 10;
     double sum  = 0;
@@ -81,9 +81,10 @@ int main(void)
 
     printf("\n * Average \t : %f \n", sum/100);
 
-    sum = 0;
+    sum = 0;*/
 
-    printf("\n -- NegExp (Drawing = 1000 000) -- \n");
+    //printf("\n -- NegExp (Drawing = 1000 000) -- \n");
+    
     /*for (int i = 0; i < 1000000; i++)
     {
         r = negExp(mean);
@@ -92,13 +93,28 @@ int main(void)
     }*/
     
     // This come close to the mean = 11
-    printf("\n * Average \t : %f \n", sum/1000000);
+    //printf("\n * Average \t : %f \n", sum/1000000);
 
     // Discrete Distribution
-    printf(" \n -- Checking discrete distribution -- \n");
+    /*printf(" \n -- Checking discrete distribution -- \n");
 
     for (int i = 0; i < 20; i++)
     {
         printf("%d-%d \t: %d \n", i, i+1, test20bins[i]);
-    }
+    }*/
+
+    //simulateRoll20DiceSum();
+
+    // Box-Muller
+
+    int test20binsBM[20] = {0};
+    int index1, index2;
+    double x1, x2;
+
+    /*for (int i = 0; i < 10000; i++)
+    {
+        genericRejectionBM(&x1, &x2, -1, 1, -1, 1);
+        printf("%f, %f \n", x1, x2);
+    }*/
+    
 }
