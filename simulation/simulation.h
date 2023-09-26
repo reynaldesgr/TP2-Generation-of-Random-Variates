f535a940bcb4d85b634a3f991eeb449b780a8cc0
Reynalde Segerie <Reynalde.SEGERIE@etu.uca.fr>
2023-09-26 11:48:39 +0200

    Revert "Update files + Box-Muller"
    This reverts commit 055c354c197a6dcdd4fda7af8707d90d66d8be86.

--- simulation/simulation.h
+++ simulation/simulation.h
@@ -1,25 +1,15 @@
 #ifndef __SIMULATION__
 #define __SIMULATION__
 
-#define NUM_SIMULATIONS 1000      // Number of simulations
-#define NUM_ROLLS 20             // Number of dice rolls
-#define NUM_BINS 120            // Number of bins for the histogram
 
 // Randomess
 double uniform(double a, double b);
 double negExp(double mean);
 double randomIndividual();
-void boxMuller(double * x1, double * x2);
-double genericRejectionBM(double * x1, double * x2, double minX, double maxX, double minY, double maxY);
-
-
-double f(double x);
 
 // Simulation
 void simulateClasses_1();
 void simulateClassesBound(double a, double b, int sizeSample, int drawings, int numberClasses);
 int  simulateHDLClass(const double * cumulativeArray, double individualPSR);
-void simulateRoll20DiceSum();
-double roll20Dice();
 
 #endif
